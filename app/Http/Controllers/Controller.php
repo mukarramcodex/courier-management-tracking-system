6eee94f93a7da2bbaaa12409bc24f2bb3c6e0e3d
Mukarram Ali <info.mukarramali@gmail.com>
2025-05-13 15:16:40 +0500

    Controller Change

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,11 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function index()
+{
+    $couriers = \App\Models\Courier::latest()->paginate(10); // You can adjust pagination
+
+    return view('couriers.index', compact('couriers'));
+}
+
 }
