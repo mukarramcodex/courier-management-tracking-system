d4db7d97a1f36d38ea00c4b6dea85fdb9de46999
Mukarram Ali <info.mukarramali@gmail.com>
2025-05-13 15:18:10 +0500

    Courier Change

--- app/Models/Courier.php
+++ app/Models/Courier.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Courier extends Model
 {
-    //
+    protected $fillable = [
+    'tracking_number',
+    'sender_name',
+    'receiver_name',
+    'origin',
+    'destination',
+    'weight',
+    'rate',
+    'status',
+    ];
 }
